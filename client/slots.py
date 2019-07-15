797002c3fa8b0287cedcbfe2ce99d03e0c94c18d
William Harrell <wharrell1@me.com>
2019-07-15 17:52:09 -0400

    improved slots to accomodate more signals

--- client/slots.py
+++ client/slots.py
@@ -2,31 +2,36 @@ from PySide2.QtCore import SIGNAL, QObject
 
 import employees_ui
 
-widgets = {
-    'employees_refresh_pushbutton': {
-        'signal': 'clicked()',
-        'fn': employees_ui.refresh_employees
-    },
-    'employees_add_pushbutton': {
-        'signal': 'clicked()',
-        'fn': employees_ui.add_employee
-    },
-    'employees_tablewidget': {
+widgets = [
+    {
+        'name': 'employees_tablewidget',
         'signal': 'cellClicked(int,int)',
         'fn': employees_ui.populate_employees_details
     },
-    'employees_delete_pushbutton': {
+    {
+        'name': 'employees_add_pushbutton',
+        'signal': 'clicked()',
+        'fn': employees_ui.add_employee
+    },
+    {
+        'name': 'employees_update_pushbutton',
+        'signal': 'clicked()',
+        'fn': employees_ui.update_employee
+    },
+    {
+        'name': 'employees_delete_pushbutton',
         'signal': 'clicked()',
         'fn': employees_ui.delete_employee
     },
-    'employees_update_pushbutton': {
+    {
+        'name': 'employees_refresh_pushbutton',
         'signal': 'clicked()',
-        'fn': employees_ui.update_employee
+        'fn': employees_ui.refresh_employees
     }
-}
+]
 
 def connect_slots(main_window):
-    for widget_name in widgets.keys():
-        widget = getattr(main_window.window, widget_name)
-        QObject.connect(widget, SIGNAL(widgets[widget_name]['signal']),
-                        widgets[widget_name]['fn'](main_window))
+    for item in widgets:
+        widget = getattr(main_window.window, item['name'])
+        QObject.connect(widget, SIGNAL(item['signal']),
+                        item['fn'](main_window))
